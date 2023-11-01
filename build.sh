91d83d573a9bdb24ca273c8ba0dc14eed9e5906b
sofiengwin <sengodwin@gmail.com>
2023-11-01 01:05:18 -0700

    update lambda after deploying to s3

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+export AWS_PROFILE=terraform
 GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o bootstrap main.go
 
 zip api_sourcer.zip bootstrap
 
-aws s3 cp ./api_sourcer.zip s3://sourcers/api_sourcer.zip --profile terraform
+aws s3 cp ./api_sourcer.zip s3://sourcers/api_sourcer.zip
+
+aws lambda update-function-code --function-name  api_sourcer --s3-bucket sourcers --s3-key api_sourcer.zip
